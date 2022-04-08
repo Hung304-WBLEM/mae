eb2ab4efcd9af95b4b741c0697d7210fd811ff48
Hung304-WBLEM <hungvq.blem.304@gmail.com>
2022-04-07 22:30:34 -0500

    update

--- main_pretrain_script.sh
+++ main_pretrain_script.sh
@@ -31,9 +31,20 @@
 #        --blr 1.5e-4 --weight_decay 0.05 \
 #        --dataset combined_datasets
 
-python -m torch.distributed.launch --master_port=6666 --nproc_per_node=4 main_pretrain_custom_v2_3branches.py \
-       --batch_size 64 \
-       --model mae_vit_base_patch16 \
+# python -m torch.distributed.launch --master_port=6666 --nproc_per_node=4 main_pretrain_custom_v2_3branches.py \
+#        --batch_size 64 \
+#        --model mae_vit_base_patch16 \
+#        --input_size 112 \
+#        --norm_pix_loss \
+#        --mask_ratio 0.75 \
+#        --epochs 800 \
+#        --warmup_epochs 40 \
+#        --blr 1.5e-4 --weight_decay 0.05 \
+#        --dataset combined_datasets
+
+python -m torch.distributed.launch --master_port=6666 --nproc_per_node=4 main_pretrain_custom_swin.py \
+       --batch_size 1 \
+       --model mae_swin_base_patch4 \
        --input_size 112 \
        --norm_pix_loss \
        --mask_ratio 0.75 \
